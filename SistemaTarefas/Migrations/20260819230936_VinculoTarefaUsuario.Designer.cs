f110f25a11f2f81740737a0556b8167c2dfe60fb
anthonidaluz <anthonidaluz@gmail.com>
2026-08-19 20:11:17 -0300

    Alterado TarefaModel e TarefaMap para vinculo com Usuario e rodado migration para atualizar o banco

--- SistemaTarefas/Migrations/20260819230936_VinculoTarefaUsuario.Designer.cs
+++ SistemaTarefas/Migrations/20260819230936_VinculoTarefaUsuario.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaTarefas.Data;
 
@@ -10,9 +11,11 @@ using SistemaTarefas.Data;
 namespace SistemaTarefas.Migrations
 {
     [DbContext(typeof(SistemaTarefasDBContext))]
-    partial class SistemaTarefasDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260819230936_VinculoTarefaUsuario")]
+    partial class VinculoTarefaUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
