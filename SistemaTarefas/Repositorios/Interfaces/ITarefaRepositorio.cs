d9b5de147a318d15106861f7c98edc1932e9c1b6
anthonidaluz <anthonidaluz@gmail.com>
2026-08-19 20:13:00 -0300

    Criado ITarefaRepositorio

---
+++ SistemaTarefas/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using SistemaTarefas.Models;
+
+namespace SistemaTarefas.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+
+        Task<List<TarefaModel>> BuscarTodasTarefas();
+        Task<TarefaModel> BuscarPorId(int id);
+        Task<TarefaModel> Adicionar(TarefaModel tarefa);
+        Task<TarefaModel> Atualizar(TarefaModel tarefa, int id);
+        Task<bool> Apagar(int id);
+    }
+}
