6753ae2ae11d1a50aa4c3c39ac50ab9af8d2d56c
anthonidaluz <anthonidaluz@gmail.com>
2026-08-19 20:54:32 -0300

    Finalizado CRUD de tarefas e vinculado com usuários

--- SistemaTarefas/Program.cs
+++ SistemaTarefas/Program.cs
@@ -22,8 +22,9 @@ namespace SistemaTarefas
                 options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
                 );
 
-            builder.Services.AddScoped<IUsuarioRepositorio, TarefaRepositorio>();
-            
+            builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+            builder.Services.AddScoped<ITarefaRepositorio, TarefaRepositorio>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
