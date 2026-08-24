2b8212da2d7b39590641e313da93507d5c5a407b
anthonidaluz <anthonidaluz@gmail.com>
2026-08-24 19:21:42 -0300

    Construido conta controller e ajustado program para gerar token

--- SistemaTarefas/Controllers/ContaController.cs
+++ SistemaTarefas/Controllers/ContaController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SistemaTarefas.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SistemaTarefas.Controllers
 {
@@ -7,5 +13,43 @@ namespace SistemaTarefas.Controllers
     [ApiController]
     public class ContaController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            if (login.Login == "admin" && login.Senha == "admin")
+            {
+                var token = GerarTokenJWT();
+                return Ok(new { token });
+            }
+
+            return BadRequest(new { mensagem = "Credenciais inválidas. Por favor, verifique seu usuário e senha" });
+        } 
+
+        private string GerarTokenJWT()
+        {
+            string chaveSecreta = "8168cc34-f954-4485-90ec-4392d9ec8a86";
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveSecreta));
+            var credencial = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] 
+            {
+                new Claim("login", "admin"),
+                new Claim("nome", "Administrador do Sistema")
+            };
+
+
+            var token = new JwtSecurityToken(
+                issuer: "sua_empresa",
+                audience: "sua_aplicacao",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credencial
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
     }
 }
